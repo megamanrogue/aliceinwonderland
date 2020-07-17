fb5ab121c4ce7921a9cf7c1547632746ee811c9e
megamanrogue <68107924+megamanrogue@users.noreply.github.com>
2020-07-18 02:13:32 +0800

    Update test_r.R

--- test_r.R
+++ test_r.R
@@ -1,1 +1,2 @@
-print("This file was created within RStudio") and then on a new line paste, print("And now it lives on GitHub")
+print("This file was created within RStudio") 
+print("And now it lives on GitHub")
